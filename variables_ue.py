5323c3f21a90b1eb2fc93adf9374bb85ce0c9ea6
utku <79229904+utkuertac@users.noreply.github.com>
2023-02-05 00:19:46 +0300

    Update variables_ue.py

--- variables_ue.py
+++ variables_ue.py
@@ -13,3 +13,20 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+x_ue = 3
+y_ue = 7.5
+name_ue = 'Utku'
+is_cool_ue = True
+
+x_ue, y_ue, name_ue, is_cool_ue = (3, 7.5, 'Utku', True)
+
+a_ue= x_ue + y_ue
+
+x_ue = str(x_ue)
+y_ue = int(y_ue)
+z_ue = float(y_ue)
+
+print(type(x_ue))
+print(type(y_ue), y_ue)
+print(type(z_ue)) 
