c2a2236eed8821e29a79bf3d782a57c460802dc8
utku <79229904+utkuertac@users.noreply.github.com>
2023-02-05 02:00:38 +0300

    Update functions_ue.py

--- functions_ue.py
+++ functions_ue.py
@@ -1,7 +1,40 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
 
+# Create function
+def sayHello(name_ue):
+  print(f'Hello {name_ue}')
+  
+  sayHello('Utku Ertaç')
+  
+  # Create function
+def  sayHello(name_ue = 'Utku'):
+  print(f'Hello {name_ue}')
 
+sayHello()
+
+# Return values
+def getSum(num1_ue, num2_ue):
+  total = num1_ue + num2_ue 
+  return total
+  
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+
+print(getSum(3, 4))
+
+# Return values
+def getSum(num1_ue, num2_ue):
+  total = num1_ue + num2_ue 
+  return total
+
+num_ue = getSum(3, 4)
+
+print(num_ue)
+
+# Lambda function 
+
+getSum = lambda num1_ue, num2_ue : num1_ue + num2_ue
+
+print(getSum(10, 3))
 
