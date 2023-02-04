e76bf097d9cb054b5316a5227257fd6974a0a408
utku <79229904+utkuertac@users.noreply.github.com>
2023-02-05 02:15:09 +0300

    Update validator_ue.py

--- validator_ue.py
+++ validator_ue.py
@@ -3,6 +3,6 @@
 import re
 
 
-def validate_email(email):
-    if len(email) > 7:
-        return bool(re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email))
+def validate_email(email_ue):
+    if len(email_ue) > 7:
+        return bool(re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email_ue))
